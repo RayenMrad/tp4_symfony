d7d53233caf0cf9f83cb30a2eb8bd7c01f28057f
Rayen Mrad <rayenmrad80@gmail.com>
2025-02-20 10:58:04 +0100

    tp2 symfony

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/{name}", name="homepage")
+     */
+    public function home($name = 'rayen'): Response
+    {
+        return $this->render('index.html.twig', ['name' => $name]);
+    }
+}
